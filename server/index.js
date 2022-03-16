ad264c45e78bcf0ba43fb3e298c6cbed108fddfb
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-16 22:03:49 +0900

    TyperacerCloneApp: Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ const app = experss();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
 
+// connect to mongodb
+const DB = 'your db';
+
+mongoose.connect(DB).then(() => {
+    console.log('Connection Successful!');
+}).catch((err) => {
+    console.log(err);
+});
+
 // listen to server
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server started and running on port ${port}`);
