2dac40aa2d42d4561fb025a842ff967d7743e6e7
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-17 23:42:48 +0900

    TyperacerCloneApp: Copy Game Code

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:typeracer_clone_app/providers/client_state_provider.dart';
 import 'package:typeracer_clone_app/providers/game_state_provider.dart';
@@ -48,6 +49,50 @@ class _GameScreenState extends State<GameScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              game.gameState['isJoin']
+                  ? ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 800),
+                      child: TextFormField(
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(
+                            text: game.gameState['id'],
+                          )).then((_) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content:
+                                    Text('Game Code copied to clickboard!'),
+                              ),
+                            );
+                          });
+                        },
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: const BorderSide(
+                              color: Colors.transparent,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: const BorderSide(
+                              color: Colors.transparent,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 14,
+                          ),
+                          fillColor: const Color(0xFFF5F5FA),
+                          hintText: 'Click to Copy Game Code',
+                          hintStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container(),
             ],
           ),
         ),
